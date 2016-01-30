ec61600463ee42dc5ba1af560dd437768de61946
Pavel Hamrik <pavel.hamrik@gmail.com>
2016-01-30 23:48:44 +0100

    Init.

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// stranger
+var stranger = require('./stranger');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+
+
+
+// post as stranger
+app.post('/hello', stranger);
+
+
+
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
